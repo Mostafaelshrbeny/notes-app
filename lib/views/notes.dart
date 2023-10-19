02b612d373a90f181d3bccc5fe5e20e9955b2e7d
mostafaelshrbeny <mostafaelshrbeny6@gmail.com>
2023-10-19 18:16:12 +0300

    add notes

--- lib/views/notes.dart
+++ lib/views/notes.dart
@@ -7,45 +7,54 @@ class Notes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    GlobalKey<FormState> formkey = GlobalKey();
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
           child: Column(
             children: [
-              Row(
-                children: [
-                  const Text(
-                    'Notes',
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  const Spacer(),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.05),
-                        borderRadius: BorderRadius.circular(16)),
-                    child: IconButton(
-                        onPressed: () {}, icon: const Icon(Icons.search)),
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Row(
+                  children: [
+                    const Text(
+                      'Notes',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    const Spacer(),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.05),
+                          borderRadius: BorderRadius.circular(16)),
+                      child: IconButton(
+                          onPressed: () {}, icon: const Icon(Icons.search)),
+                    ),
+                  ],
+                ),
               ),
               const Expanded(child: Noteslist())
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class Noteslist extends StatelessWidget {
-  const Noteslist({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) => const Noteitem(),
-      itemCount: 5,
+      floatingActionButton: Builder(builder: (context) {
+        return FloatingActionButton(
+          onPressed: () {
+            showBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32)),
+              context: context,
+              builder: (context) {
+                return Addnote(
+                  formkey: formkey,
+                );
+              },
+            );
+          },
+          child: const Icon(Icons.edit),
+        );
+      }),
     );
   }
 }
--- lib/widgets/notestools.dart
+++ lib/widgets/notestools.dart
@@ -6,7 +6,7 @@ class Noteitem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
+      margin: const EdgeInsets.symmetric(vertical: 3),
       padding: const EdgeInsets.only(left: 5, top: 10),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16), color: Colors.blue),
@@ -38,6 +38,94 @@ class Noteitem extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class Noteslist extends StatelessWidget {
+  const Noteslist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) => const Noteitem(),
+    );
+  }
+}
+
+class Addnote extends StatelessWidget {
+  const Addnote({super.key, required this.formkey});
+  final GlobalKey formkey;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 400,
+      child: Form(
+        key: formkey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 5),
+          child: Column(children: [
+            const Customtextfield(
+              label: 'Note Title',
+              hint: 'Enter your Note body',
+            ),
+            const SizedBox(height: 15),
+            const Customtextfield(
+              label: 'Note body',
+              hint: 'Enter your Note',
+              lines: 8,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                  height: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.teal,
+                      borderRadius: BorderRadius.circular(16)),
+                  width: MediaQuery.of(context).size.width,
+                  child: const Center(
+                    child: Text('Add'),
+                  )),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class Customtextfield extends StatelessWidget {
+  const Customtextfield({
+    super.key,
+    required this.label,
+    required this.hint,
+    this.lines = 1,
+  });
+  final String label;
+  final String hint;
+  final int lines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      cursorColor: Colors.teal,
+      maxLines: lines,
+      decoration: InputDecoration(
+        label: Text(label),
+        labelStyle: const TextStyle(color: Colors.white),
+        hintText: hint,
+        hintStyle: const TextStyle(color: Colors.teal),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+        enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(16)),
+        focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.teal),
+            borderRadius: BorderRadius.circular(16)),
       ),
     );
   }
